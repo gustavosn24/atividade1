5c5a5f3ba1c74f4e35be77066be091717ffcadb5
gustavosn <jose.gustavo1@escolar.ifrn.edu.br>
2026-03-30 13:47:18 -0300

    q2

--- q1.js
+++ q1.js
@@ -3,4 +3,4 @@ const pi = 3.14;
 const h = 10;
 const v = pi * (r ** 2) * h;
 
-console.log("o volume do cilindro é:" + v);
+console.log("o volume do cilindro é: " + v);
---
+++ q2.js
@@ -0,0 +1,7 @@
+const mp = 60;
+const nf = 40;
+const mf = 0;
+
+mf = (mp + nf) / 2;
+
+console.log("a média final é: " + mf);
